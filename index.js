bbf4503143e7d71177bf9086065e2d3139733c59
luist@psma.com.au <qq97qf24ffL>
2022-10-06 21:38:44 +1100

    sss

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Pipe" });
+});
+
+let server = app.listen(port, () => {
+	console.log(`Example app listening at Port: ${port}`)
+});
+
+function stop() {
+	server.close();
+}
+
+module.exports = server;
+module.exports = stop;
